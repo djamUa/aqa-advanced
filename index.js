b62957a07390376c956d3ba118beda62d8c461f4
djamUa <dzyubenko.vlad@gmail.com>
2024-07-04 16:44:43 +0300

    cd aqa-advanced

---
+++ index.js
@@ -0,0 +1,10 @@
+// const age =30;
+
+// console.log(age)
+
+var firstName = ("Vlad");
+console.log (firstName);
+
+let userAge = (30);
+let SecondName = ("Dziubenko");
+console.log (userAge,SecondName)
