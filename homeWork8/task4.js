2086b596f8c0eddf1957f3bc7e9bf9a2051fc416
djamUa <dzyubenko.vlad@gmail.com>
2024-08-07 21:26:17 +0300

    homeWork8

---
+++ homeWork8/task4.js
@@ -0,0 +1,23 @@
+// Завдання 4
+// Створіть об'єкт person з такими властивостями:
+// firstName: рядок, представляє ім'я особи.
+// lastName: рядок, представляє прізвище особи.
+// age: число, представляє вік особи.
+// Додайте до об'єкта person властивість email, яка буде містити електронну пошту особи.
+// Видаліть властивість age з об'єкта person.
+// Виведіть оновлений об'єкт person в консоль.
+
+
+
+
+const person = {
+    firstName: "Vlad",
+    lastName: "Dziubenko",
+    age: 32
+  };
+  
+  person.email = "vdtestqa@gmail.com";
+  
+  delete person.age;
+  
+  console.log(person);
